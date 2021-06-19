d31bed1e181ef64403ef80c6593b3299510aa66c
Vivek Lokhande <vivek2811999@gmail.com>
2021-06-19 16:02:07 +0530

    remove warning

--- src/products/ProductList.js
+++ src/products/ProductList.js
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
-import Filterbox from '../filter/Filterbox';
 import Checkbox from '@material-ui/core/Checkbox';
 import './productlist.css'
 
